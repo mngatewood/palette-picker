b933062e75293aeb104c5af42ca50ed2476e0596
Michael Gatewood <michael@mngatewood.com>
2018-04-30 16:29:19 -0600

    Adds random color

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,15 @@
+const colorOne = '#' + Math.floor(Math.random() * 16777215).toString(16);
+const colorTwo = '#' + Math.floor(Math.random() * 16777215).toString(16);
+const colorThree = '#' + Math.floor(Math.random() * 16777215).toString(16);
+const colorFour = '#' + Math.floor(Math.random() * 16777215).toString(16);
+const colorFive = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
+$('#header-palette-one').css('background-color', colorOne);
+$('#header-palette-two').css('background-color', colorTwo);
+$('#header-palette-three').css('background-color', colorThree);
+$('#header-palette-four').css('background-color', colorFour);
+$('#header-palette-five').css('background-color', colorFive);
 
-$("#test").click( () => {
-  alert("Handler for .click() called.");
+$('#header-button-refresh').click( () => {
+  alert('Handler for .click() called.');
 });
